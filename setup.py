3ae0b6c1109e434ec802185c9ccf7da8107b0613
JuanPablo <jpabloaj@gmail.com>
2015-01-23 21:18:36 -0300

    bump to 0.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*paths):
 
 setup(
     name="pip-init",
-    version="0.0.2",
+    version="0.0.3",
     description="pip-init to generate a base setup.py file",
     long_description=(read('README.rst')),
     license='MIT',
