f4e44a7aa7670b8fdd37cb5f9bd66d5e1cec23cf
JuanPablo <jpabloaj@gmail.com>
2015-01-27 19:18:48 -0300

    [] default value of install_requires

--- pip_init/templates.py
+++ pip_init/templates.py
@@ -4,14 +4,6 @@ from string import Template
 
 setup_base_template = Template(
     'from setuptools import setup, find_packages\n'
-    'from pip.req import parse_requirements\n'
-    'from pip.exceptions import InstallationError\n'
-    '\n'
-    'try:\n'
-    '    requirements = parse_requirements("requirements.txt")\n'
-    '    install_requires = [str(r.req) for r in requirements]\n'
-    'except InstallationError:\n'
-    '    install_requires = []\n'
     '\n'
     'try:\n'
     '    long_description = open("README.rst").read()\n'
@@ -21,7 +13,7 @@ setup_base_template = Template(
     'setup(\n'
     '${setup_lines}'
     '    packages=find_packages(),\n'
-    '    install_requires=install_requires,\n'
+    '    install_requires=[],\n'
     '    long_description=long_description\n'
     ')\n'
 )
