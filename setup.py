abb35fa01085e370639405812ca7789eade53ee7
JuanPablo <jpabloaj@gmail.com>
2016-01-30 15:36:28 -0300

    bump to 0.3.0
    * create the gitignore file.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*paths):
 
 setup(
     name="pip-init",
-    version="0.2.2",
+    version="0.3.0",
     description="pip-init to generate a base setup.py file",
     long_description=(read('README.rst')),
     license='MIT',
